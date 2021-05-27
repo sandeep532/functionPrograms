bd2aa99a8e336365ef78968fe5ce79c5a5a184b7
sandeep532 <sandeepsharmadurbhaka@gmail.com>
2021-05-27 23:23:24 +0530

    To find distance

---
+++ Distance.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Distance
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the values of x and y");
+int x=sc.nextInt();
+int y=sc.nextInt();
+System.out.println("\nDistance is :"+Math.sqrt(Math.pow(x,2)+Math.pow(y,2)));
+
+}
+}
